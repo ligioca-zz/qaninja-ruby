e933d804dd608b7603a46277aaa53c4580344484
Ligia Almeida <Ligiox@Ligias-Air.home>
2019-06-01 23:31:30 +0200

    changing

--- advanced/classes.rb
+++ advanced/classes.rb
@@ -1,10 +1,18 @@
 class Conta
 
+    attr_accessor :saldo
+
     def deposita(valor)
-        puts 'Depositando o valor #{valor}'
+        self.saldo += valor
+        puts "Depositando o valor #{valor}" 
     end
 
 end
 
 c = Conta.new
+# puts c.saldo.class
+c.saldo = 0.0
+puts "Saldo antes: #{c.saldo}"
 c.deposita(100.00)
+puts "Saldo depois: #{c.saldo}"
+
